152ad704d3da56fad5bcc166e62080f2901027e5
isaacfadzo <g00372043@gmit.ie>
2020-01-20 17:38:34 +0000

    addedforloop

--- src/ie/gmit/packagename/HelloWorld.java
+++ src/ie/gmit/packagename/HelloWorld.java
@@ -4,7 +4,12 @@ class HelloWorld {
 
     public static void main(String[] args) {
         // Print Hello World! to the console.
-        System.out.println("Hello World!");
+        System.out.println("Hello Isaac");
+      
+        for (int i = 0; i < 10; i++) {  
+            System.out.println("Hello Isaac");
+      } 
     }
+ }
 
-}
+
